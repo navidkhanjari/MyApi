f46d7289b548297cae77c2c8236dcc4c7118b555
navidkhanjari <navidkhanjari72@gmail.com>
2024-06-05 09:20:50 +0330

    Entities Layer

---
+++ Entities/Post/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities
+{
+    public class Category : BaseEntity
+    {
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+        public int? ParentCategoryId { get; set; }
+
+        [ForeignKey(nameof(ParentCategoryId))]
+        public Category ParentCategory { get; set; }
+        public ICollection<Category> ChildCategories { get; set; }
+        public ICollection<Post> Posts { get; set; }
+    }
+}
---
+++ Entities/User/Role.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Entities
+{
+    public class Role : IEntity
+    {
+        [Required]
+        [StringLength(100)]
+        public string Description { get; set; }
+    }
+}
